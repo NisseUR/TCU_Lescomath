b82451c7b0778c496c334380a0306fedcf3e9346
nisse2112 <denisse.ugalde@ucr.ac.cr>
2024-03-02 15:54:53 -0600

    mejoras a la pagina

--- LescoMath/src/components/ContentBox/Boxes.js
+++ LescoMath/src/components/ContentBox/Boxes.js
@@ -1,22 +1,40 @@
 import React from "react";
 import ContentBox from './ContentBox.js';
-import './Boxes.css'
+import './Boxes.css';
 
 function Boxes() {
+  const repositories = [
+    {
+      boxName: "Números",
+      boxUrl: "https://www.youtube.com/watch?v=0MQly46xEuk",
+      topics: [
+        "Números Naturales",
+        "Operaciones: Suma, Resta, Multiplicación, División, Potencias",
+        "Combinación de operaciones",
+        // ... más temas ...
+      ],
+    },
+    {
+      boxName: "Fracciones",
+      boxUrl: "https://www.youtube.com/watch?v=xxxxx",
+      topics: [
+        "Concepto de fracción",
+        "Suma y resta de fracciones",
+        // ... más temas ...
+      ],
+    },
+    // ... más cajas ...
+  ];
 
-    const repositories = [
-      {boxName: "Números", boxUrl: "https://www.youtube.com/watch?v=0MQly46xEuk"}, 
-    ];
-  
-    return (
-      <div className="boxes_containers">
-        <div className="box-container"> {/**/}
-          {repositories.map(box => (
-            <ContentBox key={box.boxName} repoName={box.boxName} repoUrl={box.boxUrl} />
-          ))}
-        </div>
+  return (
+    <div className="boxes_containers">
+      <div className="box-container">
+        {repositories.map((box, index) => (
+          <ContentBox key={index} repoName={box.boxName} repoUrl={box.boxUrl} topics={box.topics} />
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
+
 export default Boxes;
--- LescoMath/src/components/ContentBox/ContentBox.js
+++ LescoMath/src/components/ContentBox/ContentBox.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import './ContentBox.css'
+import React, { useState } from 'react';
+import './ContentBox.css';
 
-function ContentBox({ repoName, repoUrl }) {
+function ContentBox({ repoName, repoUrl, topics }) {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleToggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
     <div className="content-box">
       <h2>{repoName}</h2>
       <a href={repoUrl} target="_blank" rel="noopener noreferrer">Abrir el video</a>
+      <button onClick={handleToggleDetails}>Mostrar Temas</button>
+      {showDetails && (
+        <div className="topic-details">
+          <ul>
+            {topics.map((topic, index) => (
+              <li key={index}>{topic}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
 
 export default ContentBox;
+
